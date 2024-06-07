7335b931cac77eb80b31c19b43cad1cb190b19c2
Dinesh Shaw <idineshshaw7@gmail.com>
2024-06-07 13:31:03 +0530

    Initial Commit

---
+++ SwiftUI_Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI_Practice
+//
+//  Created by Dinesh Shaw on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
